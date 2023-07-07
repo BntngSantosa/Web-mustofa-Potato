bb138768503f221c7d512185ab25bda75ccd9231
BntngSantosa <bintangsantosa11@gmail.com>
2023-07-07 14:26:39 +0700

    Update responsive web

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,15 @@
-// const bars = document.getElementById("bars");
-// const sideBar = document.querySelector(".container ul");
-// bars.addEventListener("click", (e) => {
-//   sideBar.style.right = "0";
-//   if(e.target) return sideBar.style.
-// });
+function navbar() {
+  const menu = document.querySelector("#bars");
+  const navbar = document.querySelector(".container ul");
 
-const menu = document.querySelector("#bars");
-const navbar = document.querySelector(".container ul");
+  menu.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+  });
 
-menu.addEventListener("click", () => {
-  navbar.classList.toggle("active")
-});
-
-document.addEventListener("click", (e) => {
-  if(!menu.contains(e.target) && !navbar.contains(e.target)){
-    navbar.classList.remove("active");
-  }
-});
+  document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !navbar.contains(e.target)) {
+      navbar.classList.remove("active");
+    }
+  });
+}
+navbar();
